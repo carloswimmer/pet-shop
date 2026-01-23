53a97a6a82d53a3b011d727995d21cb30bc87a8e
Carlos Wimmer <coiote777@gmail.com>
2026-01-23 14:42:52 +0100

    Start styling Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
 export default function Home() {
   return (
-    <div>
-      <h2 className='text-accent-blue text-title'>Hello World</h2>
+    <div className='bg-background-primary p-6'>
+      <div className='flex items-center justify-between md:m-8'>
+        <div>
+          <h1 className='text-title text-content-primary mb-2'>Your Agenda</h1>
+          <p className='text-paragraph-small text-content-secondary'>
+            Here you can see all the clients and services scheduled for today.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
