65bd02f91bf39f543444b2432819b4f39eb4c519
Carlos Wimmer <coiote777@gmail.com>
2026-01-25 14:23:49 +0100

    Create input phone

--- src/components/appointment-form/appointment-form.tsx
+++ src/components/appointment-form/appointment-form.tsx
@@ -1,8 +1,9 @@
 'use client';
 
 import { zodResolver } from '@hookform/resolvers/zod';
-import { Divide, Dog, User } from 'lucide-react';
+import { Dog, Phone, User } from 'lucide-react';
 import { useForm } from 'react-hook-form';
+import { IMaskInput } from 'react-imask';
 import type z from 'zod';
 import { Button } from '@/components/ui/button';
 import {
@@ -79,6 +80,7 @@ export const AppointmentForm = () => {
                       <Input
                         placeholder="Tutor name"
                         className="pl-10"
+                        autoComplete="name"
                         {...field}
                       />
                     </div>
@@ -106,6 +108,37 @@ export const AppointmentForm = () => {
                         placeholder="Pet name"
                         className="pl-10"
                         {...field}
+                      />
+                    </div>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="phone"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-label-medium-size text-content-primary">
+                    Phone number
+                  </FormLabel>
+                  <FormControl>
+                    <div className="relative">
+                      <Phone
+                        className="absolute left-3 top-1/2 -translate-y-1/2 transform text-content-brand"
+                        size={20}
+                      />
+                      <IMaskInput
+                        placeholder="(999) 999-9999"
+                        mask="(000) 000-0000"
+                        className="pl-10 flex h-12 w-full rounded-md border border-border-primary bg-background-tertiary px-3 py-2 text-sm text-content-primary ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-content-secondary focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-offset-0 focus-visible:ring-border-brand disabled:cursor-not-allowed disabled:opacity-50 hover:border-border-secondary focus:border-border-brand focus-visible:border-border-brand aria-invalid:ring-destructive/20 aria-invalid:border-destructive"
+                        value={field.value}
+                        unmask={true}
+                        onAccept={(value) => field.onChange(value)}
+                        onBlur={field.onBlur}
+                        autoComplete="tel"
                       />
                     </div>
                   </FormControl>
--- src/schema/appointmentFormSchema.ts
+++ src/schema/appointmentFormSchema.ts
@@ -3,6 +3,6 @@ import z from 'zod';
 export const appointmentFormSchema = z.object({
   tutorName: z.string().min(3, 'The tutor name is required'),
   petName: z.string().min(3, 'The pet name is required'),
-  phone: z.string().min(11, 'The phone number is required'),
+  phone: z.string().min(10, 'The phone number is required'),
   description: z.string().min(3, 'The service description is required'),
 });
