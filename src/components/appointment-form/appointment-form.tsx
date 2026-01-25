796a1552e817fb990b1123b1e91ef17cc787db5f
Carlos Wimmer <coiote777@gmail.com>
2026-01-25 13:07:47 +0100

    Create appointment schema

--- src/components/appointment-form/appointment-form.tsx
+++ src/components/appointment-form/appointment-form.tsx
@@ -1,3 +1,8 @@
+'use client';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import type z from 'zod';
 import { Button } from '@/components/ui/button';
 import {
   Dialog,
@@ -7,21 +12,51 @@ import {
   DialogTitle,
   DialogTrigger,
 } from '@/components/ui/dialog';
+import { Form } from '@/components/ui/form';
+import { appointmentFormSchema } from '@/schema/appointmentFormSchema';
+
+type AppointmentFormValues = z.infer<typeof appointmentFormSchema>;
 
 export const AppointmentForm = () => {
+  const form = useForm<AppointmentFormValues>({
+    resolver: zodResolver(appointmentFormSchema),
+    defaultValues: {
+      tutorName: '',
+      petName: '',
+      phone: '',
+      description: '',
+    },
+  });
+
+  const onSubmit = (data: AppointmentFormValues) => {
+    console.log(data);
+  };
+
   return (
     <Dialog>
-      <DialogTrigger>
+      <DialogTrigger asChild>
         <Button variant="brand">New appointment</Button>
       </DialogTrigger>
 
-      <DialogContent>
+      <DialogContent
+        variant="appointment"
+        overlayVariant="blurred"
+        showCloseButton
+      >
         <DialogHeader>
-          <DialogTitle>Book an appointment</DialogTitle>
-          <DialogDescription>
+          <DialogTitle size="modal">Book an appointment</DialogTitle>
+          <DialogDescription size="modal">
             Fill up the user data to create an appointment
           </DialogDescription>
         </DialogHeader>
+
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <input type="text" {...form.register('tutorName')} />
+
+            <Button type="submit">Save</Button>
+          </form>
+        </Form>
       </DialogContent>
     </Dialog>
   );
